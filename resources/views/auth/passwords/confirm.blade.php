822e890a40687e99d0f12744ced7536ae21ce105
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-21 22:15:17 +0100

    refact: put home nav into partials

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+@include('partials.nav')
     <div class="container mx-auto">
         <div class="flex flex-wrap justify-center">
             <div class="w-full max-w-sm">
