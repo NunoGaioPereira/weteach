57be20fc9b9fd7354145261d03c0a986d7f24203
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-24 19:56:27 +0100

    feat: add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -249,57 +249,7 @@
     </div>
     <!-- End Pricing Section -->
 
-
-    <footer class="bg-black text-white pt-12 pb-8 px-4">
-        <div class="mx-auto px-4 container overflow-hidden max-w-6xl flex flex-col lg:flex-row justify-between">
-            <a href="/" class="block mr-4 w-1/3">
-                <span class="flex items-center">
-                    <svg class="h-6 w-auto fill-current text-white" viewBox="0 0 281 280" height="280" xmlns="http://www.w3.org/2000/svg"><path d="M140 0C89.592-.006 43.073 27.088 18.206 70.935-6.66 114.78-6.037 168.61 19.84 211.87a135.373 135.373 0 006.73 10.2c40.925 56.523 117.212 74.304 178.91 41.7l-11.29-19.51c-42.055 21.797-93.087 16.206-129.426-14.178-36.34-30.384-50.88-79.618-36.875-124.869C41.893 59.963 81.7 27.547 128.85 23c3.67-.34 7.39-.52 11.15-.52 3.76 0 7.47.18 11.14.52 42.314 4.012 79.16 30.57 96.344 69.444 17.185 38.875 12.028 84-13.484 117.996L140 47.9 52.19 200h33.87L140 106.56l72.75 125.69 11.38 19.66c48.149-36.192 67.751-99.125 48.67-156.258C253.72 38.52 200.235-.004 140 0z"/></svg>
-                    <span class="font-black ml-2 text-lg">SAAS</span>
-                </span>
-            </a>
-            <div class="w-2/3 block sm:flex text-sm mt-6 lg:mt-0">
-                <ul class="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
-                    <li class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Product</li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Features</a></li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Integrations</a></li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Pricing</a></li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">FAQ</a></li>
-                </ul>
-                <ul class="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
-                    <li class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Company</li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Privacy</a>
-                    </li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Terms of Service</a></li>
-                </ul>
-                <ul class="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
-                    <li class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Developers</li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Developer API</a></li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Documentation</a></li>
-                    <li><a href="#_" class="inline-block py-2 px-3 text-gray-500 hover:text-white no-underline">Guides</a></li>
-                    </li>
-                </ul>
-                <div class="text-gray-700 flex flex-col w-full">
-                    <div class="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Follow Us</div>
-                    <div class="flex pl-4 justify-start mt-2">
-                        <a class="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" target="_blank" href="https://devdojo.com">
-                            <svg viewBox="0 0 24 24" class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M23.998 12c0-6.628-5.372-12-11.999-12C5.372 0 0 5.372 0 12c0 5.988 4.388 10.952 10.124 11.852v-8.384H7.078v-3.469h3.046V9.356c0-3.008 1.792-4.669 4.532-4.669 1.313 0 2.686.234 2.686.234v2.953H15.83c-1.49 0-1.955.925-1.955 1.874V12h3.328l-.532 3.469h-2.796v8.384c5.736-.9 10.124-5.864 10.124-11.853z" /></svg>
-                        </a>
-                        <a class="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" target="_blank" href="https://devdojo.com">
-                            <svg viewBox="0 0 24 24" class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M23.954 4.569a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.691 8.094 4.066 6.13 1.64 3.161a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.061a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.937 4.937 0 004.604 3.417 9.868 9.868 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63a9.936 9.936 0 002.46-2.548l-.047-.02z" /></svg>
-                        </a>
-                        <a class="block flex items-center text-gray-300 hover:text-white no-underline" target="_blank" href="https://devdojo.com">
-                            <svg viewBox="0 0 24 24" class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="pt-4 mt-10 pt-6 text-gray-600 border-t border-gray-800 text-center"> © 2020 SAAS App. All rights reserved.</div>
-    </footer>
+    @include('partials.footer')
 
     <!-- Just a little JS for the slider (#How Does It Work) section -->
     <script>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,13 +5,16 @@
 @section('content') 
     
     @include('partials.nav')
-    <div class="bg-gray-200">
-    <div class="container mx-auto lg:max-w-4xl px-5 pt-20">
-        <h1 class="font-bold text-4xl mb-4">{{ $page->title }}</h1>
+    <div class="bg-gray-200 pb-12">
+        <div class="container mx-auto lg:max-w-4xl px-5 pt-20">
+            <h1 class="font-bold text-4xl mb-4">{{ $page->title }}</h1>
 
-        <div class="bg-white px-8 py-6 border-t-4 border-indigo-500 page">
-            {!! $page->body !!}
-        </div>      
+            <div class="bg-white px-8 py-6 border-t-4 border-indigo-500 page">
+                {!! $page->body !!}
+            </div>      
+        </div>
     </div>
-    </div>
+
+    @include('partials.footer')
+
 @endsection
