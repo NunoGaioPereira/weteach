89cfff82b2d043c95a74b87adf23c1a4b25b3f4c
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-24 11:03:48 +0100

    fix: only have switch plan modal in DOM if user is already subscribed

--- resources/views/settings/billing.blade.php
+++ resources/views/settings/billing.blade.php
@@ -10,18 +10,20 @@
         
         @include('settings.nav')
 
-        <div id="switch-plans-modal" class="fixed w-full h-full inset-0 z-50">
-            <div class="fixed opacity-50 bg-black inset-0 w-full h-full"></div>
-            <form method="POST" action="{{ route('billing.switch_plan') }}" class="absolute bg-white rounded-lg p-5" id="switch-plans">
-                @csrf
-                <div id="switch-plans-close" class="absolute right-0 top-0 -mt-4 -mr-4 w-8 h-8 rounded-full shadow bg-white flex justify-center align-center text-xl font-bold cursor-pointer">&times;</div>
-                <p class="text-sm text-gray-600 mb-4">Switch Your Plan</p>
-                @include('partials.plans')
-                <button class="bg-indigo-500 text-white text-sm font-medium px-6 py-2 rounded float-right uppercase cursor-pointer">
-                    Switch Plan
-                </button>
-            </form>
-        </div>
+        @if(auth()->user()->subscribed('main'))
+            <div id="switch-plans-modal" class="fixed w-full h-full inset-0 z-50">
+                <div class="fixed opacity-50 bg-black inset-0 w-full h-full"></div>
+                <form method="POST" action="{{ route('billing.switch_plan') }}" class="absolute bg-white rounded-lg p-5" id="switch-plans">
+                    @csrf
+                    <div id="switch-plans-close" class="absolute right-0 top-0 -mt-4 -mr-4 w-8 h-8 rounded-full shadow bg-white flex justify-center align-center text-xl font-bold cursor-pointer">&times;</div>
+                    <p class="text-sm text-gray-600 mb-4">Switch Your Plan</p>
+                    @include('partials.plans')
+                    <button class="bg-indigo-500 text-white text-sm font-medium px-6 py-2 rounded float-right uppercase cursor-pointer">
+                        Switch Plan
+                    </button>
+                </form>
+            </div>
+        @endif
 
         <form action="{{ route('billing.save') }}" id="billing-form" method="POST" enctype="multipart/form-data">
             @csrf
